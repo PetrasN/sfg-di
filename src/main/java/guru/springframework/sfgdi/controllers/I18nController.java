502c4416ba7eb3d3f789159ce8afc94888be87f0
Dell <petrasnefas@gmail.com>
2020-03-24 18:17:30 +0200

    Internalizacija

---
+++ src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class I18nController {
+private  final GreetingService greetingService;
+
+    public I18nController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+    public String sayHello(){
+     return   greetingService.sayGreeting();
+    }
+}
