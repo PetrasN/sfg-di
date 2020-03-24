6ae8bf8325127b857499100f2a6d0f97640d54d0
Dell <petrasnefas@gmail.com>
2020-03-24 19:10:34 +0200

    Internalizacija

--- src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
@@ -1,13 +1,14 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class I18nController {
 private  final GreetingService greetingService;
 
-    public I18nController(GreetingService greetingService) {
+    public I18nController(@Qualifier("i118nService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
     public String sayHello(){
